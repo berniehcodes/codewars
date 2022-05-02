403f775b99a5576b280e947d1027d38f4e44b91e
berniehcodes <coding.bernie@gmail.com>
2022-05-02 12:11:25 -0600

    Coding challenge and solution for 5-02-22

---
+++ js/CC-2022-05-02/codewars_05-02-2022.js
@@ -0,0 +1,7 @@
+//Remove first and last character (8kyu)
+function removeChar(str){
+    //remove first string at [0] and last string [length-1]
+   
+     return str.slice(1,str.length-1)
+   };
+   
